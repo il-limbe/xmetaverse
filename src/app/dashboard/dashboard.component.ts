f1874385e072192b50a53b779e48299b115786b2
Andrea Limberti <a.limberti@reply.it>
2022-03-05 15:38:57 +0100

    Dashboard update grid

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -21,13 +21,11 @@ export class DashboardComponent {
       }
 
       return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
+        { title: 'Skills', cols: 1, rows: 2 },
+        { title: 'Wolverine', cols: 2, rows: 2 },
       ];
     })
   );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver) { }
 }
